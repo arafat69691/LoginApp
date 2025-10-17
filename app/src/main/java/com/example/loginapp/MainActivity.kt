1588e31ad094a141c30a29e3c1103355d4c5a0cd
Yeasin Arafat <aparafat81@gmail.com>
2025-10-18 00:13:40 +0600

    Create Intent

--- app/src/main/java/com/example/loginapp/MainActivity.kt
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -1,6 +1,8 @@
  package com.example.loginapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnIntent = findViewById<Button>(R.id.signUpBtn)
+        btnIntent.setOnClickListener {
+            intent = Intent(applicationContext, SignIn ::class.java)
+            startActivity(intent)
         }
     }
 }
